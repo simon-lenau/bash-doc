2c1e7046fb303776a3cde80799aef4f150f46fe4
Simon Lenau <lenau@cispa.dem>
2024-04-17 09:56:19 +0200

    Fix newline in argument descriptions

--- Test.sh
+++ Test.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # Your string containing newlines
-string="line1
-line2
-line3"
+string="https://bla.bla/eins/zwei/drei/"
+stringb="bla.bla/eins/zwei/drei/"
 
 # Use awk to split the string at newlines, add an initial character to each line, and print
-echo "$string" | awk '{print "Prefix: " $0}'
+# echo "$string" |
+#     sed \
+#         -e 's/[\/:]/;/g' |
+#         sed \
+#         -e 's/;\{2,\}/;/g'  #| awk -F\[\;\] -v f=1 -v t=2 '{for(i=f;i<=t;i++) printf("%s%s",$i,(i==t)?"\n":OFS)}'
+
+echo "$string" |
+    sed \
+        -e 's/\([^\/]\)[\/][^\/].*/\1/g' 
+        # -e 's/\([^\/]*[\/]\{2,\}[^\/]*\).*$/>\1</g' 
+        # -e 's/\([^\/]*[\/]\{2,\}[^\/]*\).*$/>\1</g' 
